a8517ce8c1e6c38d34f6103584330f1d58801ba6
Dong Ki Kim <dkkim93@mit.edu>
2018-09-24 00:18:54 -0400

    Dong-Ki updated: * Simple.py updated such that inital landmark and agent position is sufficiently far

--- multiagent/scenarios/simple.py
+++ multiagent/scenarios/simple.py
@@ -2,6 +2,7 @@ import numpy as np
 from multiagent.core import World, Agent, Landmark
 from multiagent.scenario import BaseScenario
 
+
 class Scenario(BaseScenario):
     def make_world(self):
         world = World()
@@ -24,19 +25,34 @@ class Scenario(BaseScenario):
     def reset_world(self, world):
         # random properties for agents
         for i, agent in enumerate(world.agents):
-            agent.color = np.array([0.25,0.25,0.25])
+            agent.color = np.array([0.25, 0.25, 0.25])
         # random properties for landmarks
         for i, landmark in enumerate(world.landmarks):
-            landmark.color = np.array([0.75,0.75,0.75])
-        world.landmarks[0].color = np.array([0.75,0.25,0.25])
+            landmark.color = np.array([0.75, 0.75, 0.75])
+        world.landmarks[0].color = np.array([0.75, 0.25, 0.25])
         # set random initial states
         for agent in world.agents:
-            agent.state.p_pos = np.random.uniform(-1,+1, world.dim_p)
+            agent.state.p_pos = np.random.uniform(-1, +1, world.dim_p)
             agent.state.p_vel = np.zeros(world.dim_p)
             agent.state.c = np.zeros(world.dim_c)
         for i, landmark in enumerate(world.landmarks):
-            landmark.state.p_pos = np.random.uniform(-1,+1, world.dim_p)
+            landmark.state.p_pos = np.random.uniform(-1, +1, world.dim_p)
+            # dk: Check the distance between agent and landmark are initialized
+            # at least x distance
+            while self.check_distance(world.agents, landmark.state.p_pos):
+                landmark.state.p_pos = np.random.uniform(-1, +1, world.dim_p)
             landmark.state.p_vel = np.zeros(world.dim_p)
+
+    def check_distance(self, agents, landmark_p_pos):
+        assert len(agents) == 1
+
+        threshold = 1.
+            
+        for i_agt, agent in enumerate(agents):
+            if np.linalg.norm(agent.state.p_pos - landmark_p_pos) < threshold:
+                return True
+            else:
+                return False
 
     def reward(self, agent, world):
         dist2 = np.sum(np.square(agent.state.p_pos - world.landmarks[0].state.p_pos))
