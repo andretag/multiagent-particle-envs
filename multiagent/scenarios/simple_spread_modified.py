5f440ee015e7371e1bd6e8450c02748b8fcfaf3a
Dong Ki Kim <dkkim93@mit.edu>
2018-09-28 19:42:10 -0400

    Dong-Ki updated: * Smaller agent size soely for better vis. Should not affect reward function and others

--- multiagent/scenarios/simple_spread_modified.py
+++ multiagent/scenarios/simple_spread_modified.py
@@ -17,7 +17,7 @@ class Scenario(BaseScenario):
             agent.name = 'agent %d' % i
             agent.collide = False
             agent.silent = True
-            agent.size = 0.15
+            agent.size = 0.10
         # add landmarks
         world.landmarks = [Landmark() for i in range(num_landmarks)]
         for i, landmark in enumerate(world.landmarks):
