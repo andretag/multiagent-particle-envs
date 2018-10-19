d538df9ca9f797032edd878e4fcdd56b6fee21ef
Dong Ki Kim <dkkim93@mit.edu>
2018-10-19 14:10:17 -0400

    Dong-Ki updated: * Param update to have enough gap between the boxes

--- multiagent/scenarios/complex_push.py
+++ multiagent/scenarios/complex_push.py
@@ -81,13 +81,13 @@ class Scenario(BaseScenario):
             landmark.state.p_vel = np.zeros(world.dim_p)
 
             if "box" in landmark.name and landmark.index == 0:
-                landmark.state.p_pos = np.array([-0.30, 0.0])
+                landmark.state.p_pos = np.array([-0.35, 0.0])
             elif "box" in landmark.name and landmark.index == 1:
-                landmark.state.p_pos = np.array([+0.30, 0.0])
+                landmark.state.p_pos = np.array([+0.35, 0.0])
             elif "target" in landmark.name and landmark.index == 0:
-                landmark.state.p_pos = np.array([-0.80, 0.0])
+                landmark.state.p_pos = np.array([-0.85, 0.0])
             elif "target" in landmark.name and landmark.index == 1:
-                landmark.state.p_pos = np.array([+0.80, 0.0])
+                landmark.state.p_pos = np.array([+0.85, 0.0])
             else:
                 raise ValueError()
 
