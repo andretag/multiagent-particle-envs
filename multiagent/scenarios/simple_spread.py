b64ae2529b64922378d9194fa499859dad9df961
dongki <dkim930122@ttic.edu>
2018-11-15 20:49:08 -0500

    * Color added
    * analysis mode added
    
    * Initial loc update

--- multiagent/scenarios/simple_spread.py
+++ multiagent/scenarios/simple_spread.py
@@ -4,13 +4,16 @@ from multiagent.scenario import BaseScenario
 
 
 class Scenario(BaseScenario):
-    def make_world(self):
+    def make_world(self, analysis_mode=-1):
         world = World()
+        self.analysis_mode = analysis_mode
+
         # set any world properties first
         world.dim_c = 2
         num_agents = 2
         num_landmarks = 2
         world.collaborative = True
+
         # add agents
         world.agents = [Agent() for i in range(num_agents)]
         for i, agent in enumerate(world.agents):
@@ -18,12 +21,14 @@ class Scenario(BaseScenario):
             agent.collide = False
             agent.silent = True
             agent.size = 0.15
+
         # add landmarks
         world.landmarks = [Landmark() for i in range(num_landmarks)]
         for i, landmark in enumerate(world.landmarks):
             landmark.name = 'landmark %d' % i
             landmark.collide = False
             landmark.movable = False
+
         # make initial conditions
         self.reset_world(world)
         return world
@@ -31,7 +36,12 @@ class Scenario(BaseScenario):
     def reset_world(self, world):
         # random properties for agents
         for i, agent in enumerate(world.agents):
-            agent.color = np.array([0.35, 0.35, 0.85])
+            if i == 0:
+                agent.color = np.array([1.0, 0.0, 0.0])
+            elif i == 1:
+                agent.color = np.array([0.0, 1.0, 0.0])
+            else:
+                raise ValueError()
 
         # random properties for landmarks
         for i, landmark in enumerate(world.landmarks):
@@ -49,13 +59,31 @@ class Scenario(BaseScenario):
 
         for i, agent in enumerate(world.agents):
             if i == 0:
-                agent.state.p_pos = np.array([-0.2, 0.0])
+                agent.state.p_pos = np.array([-0.30, 0.0])
             elif i == 1:
-                agent.state.p_pos = np.array([+0.2, 0.0])
+                agent.state.p_pos = np.array([+0.30, 0.0])
             else:
                 raise ValueError()
             agent.state.p_vel = np.zeros(world.dim_p)
             agent.state.c = np.zeros(world.dim_c)
+
+        # Analysis mode
+        if self.analysis_mode == -1:
+            pass
+        elif self.analysis_mode == 0:
+            world.agents[0].state.p_pos = np.random.uniform(-1., +1., world.dim_p)
+            world.agents[1].state.p_pos = np.array([+0.5, 0.])
+        elif self.analysis_mode == 1:
+            world.agents[0].state.p_pos = np.random.uniform(-1., +1., world.dim_p)
+            world.agents[1].state.p_pos = np.array([-0.5, 0.])
+        elif self.analysis_mode == 2:
+            world.agents[0].state.p_pos = np.array([+0.5, 0.])
+            world.agents[1].state.p_pos = np.random.uniform(-1., +1., world.dim_p)
+        elif self.analysis_mode == 3:
+            world.agents[0].state.p_pos = np.array([-0.5, 0.])
+            world.agents[1].state.p_pos = np.random.uniform(-1., +1., world.dim_p)
+        else:
+            raise ValueError()
 
     def benchmark_data(self, agent, world):
         rew = 0
