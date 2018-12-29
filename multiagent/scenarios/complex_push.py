200c84958ab2ea43bf36a48744a7b974360116c2
dongki <dkim930122@ttic.edu>
2018-12-29 15:07:23 -0500

    Revert back to 502079eec

--- multiagent/scenarios/complex_push.py
+++ multiagent/scenarios/complex_push.py
@@ -15,8 +15,9 @@ class Scenario(BaseScenario):
             agent.silent = True
             agent.size = 0.1
 
+        n_box = 1  # One box and pushing to left
+
         # add boxes
-        n_box = 1  # NOTE One box and pushing to left
         self.boxes = [Landmark() for _ in range(n_box)]
         for i, box in enumerate(self.boxes):
             box.name = 'box %d' % i
@@ -28,7 +29,7 @@ class Scenario(BaseScenario):
             world.landmarks.append(box)
 
         # add targets
-        self.targets = [Landmark() for _ in range(n_box)]
+        self.targets = [Landmark() for _ in range(2)]
         for i, target in enumerate(self.targets):
             target.name = 'target %d' % i
             target.collide = False
@@ -72,6 +73,8 @@ class Scenario(BaseScenario):
                 landmark.state.p_pos = np.array([-0.25, 0.0])
             elif "target" in landmark.name and landmark.index == 0:
                 landmark.state.p_pos = np.array([-0.85, 0.0])
+            elif "target" in landmark.name and landmark.index == 1:
+                landmark.state.p_pos = np.array([+0.85, 0.0])
             else:
                 raise ValueError()
 
@@ -87,6 +90,8 @@ class Scenario(BaseScenario):
                 box0 = landmark
             elif "target" in landmark.name and landmark.index == 0:
                 target0 = landmark
+            elif "target" in landmark.name and landmark.index == 1:
+                target1 = landmark
             else:
                 raise ValueError()
 
