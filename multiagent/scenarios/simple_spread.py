23520ed56defb854dfcc4731a0713cdb232f5e4a
Dong Ki Kim <dkkim93@mit.edu>
2019-04-23 09:41:00 -0400

    Simple spread cleanup and added check target dist

--- multiagent/scenarios/simple_spread.py
+++ multiagent/scenarios/simple_spread.py
@@ -5,91 +5,85 @@ from multiagent.scenario import BaseScenario
 
 class Scenario(BaseScenario):
     def make_world(self):
+        """Define two agents and two targets"""
         world = World()
-        # set any world properties first
-        world.dim_c = 2
-        num_agents = 2
-        num_landmarks = num_agents
+        world.dim_c = 2  # Communication
         world.collaborative = True
-        # add agents
-        world.agents = [Agent() for i in range(num_agents)]
+
+        world.agents = [Agent() for i in range(2)]
         for i, agent in enumerate(world.agents):
             agent.name = 'agent %d' % i
             agent.collide = True
             agent.silent = True
             agent.size = 0.15
-        # add landmarks
-        world.landmarks = [Landmark() for i in range(num_landmarks)]
+
+        world.landmarks = [Landmark() for i in range(2)]
         for i, landmark in enumerate(world.landmarks):
             landmark.name = 'landmark %d' % i
             landmark.collide = False
             landmark.movable = False
-        # make initial conditions
+
         self.reset_world(world)
+
         return world
 
     def reset_world(self, world):
-        # random properties for agents
+        """Initialize agents with random location.
+        But, for targets, ensure enough distance between
+        the two targets
+        """
         for i, agent in enumerate(world.agents):
-            agent.color = np.array([0.35, 0.35, 0.85])
-        # random properties for landmarks
-        for i, landmark in enumerate(world.landmarks):
-            landmark.color = np.array([0.25, 0.25, 0.25])
-        # set random initial states
-        for agent in world.agents:
-            agent.state.p_pos = np.random.uniform(-1, +1, world.dim_p)
+            if i == 0:
+                agent.color = np.array([1., 0., 0.])  # Red
+            elif i == 1:
+                agent.color = np.array([0., 1., 0.])  # Green
+            else:
+                raise ValueError("Only two agents are supported")
+            agent.state.p_pos = np.random.uniform(-1., +1., world.dim_p)
             agent.state.p_vel = np.zeros(world.dim_p)
             agent.state.c = np.zeros(world.dim_c)
+
         for i, landmark in enumerate(world.landmarks):
-            landmark.state.p_pos = np.random.uniform(-1, +1, world.dim_p)
+            landmark.color = np.array([0.25, 0.25, 0.25])
+            landmark.state.p_pos = np.random.uniform(-1., +1., world.dim_p)
             landmark.state.p_vel = np.zeros(world.dim_p)
 
-    def benchmark_data(self, agent, world):
-        rew = 0
-        collisions = 0
-        occupied_landmarks = 0
-        min_dists = 0
-        for l in world.landmarks:
-            dists = [np.sqrt(np.sum(np.square(a.state.p_pos - l.state.p_pos))) for a in world.agents]
-            min_dists += min(dists)
-            rew -= min(dists)
-            if min(dists) < 0.1:
-                occupied_landmarks += 1
-        if agent.collide:
-            for a in world.agents:
-                if self.is_collision(a, agent):
-                    rew -= 1
-                    collisions += 1
-        return (rew, collisions, min_dists, occupied_landmarks)
+        # Check target distances
+        while self.check_target_dist(world, th=agent.size * 5) is False:
+            for i, landmark in enumerate(world.landmarks):
+                landmark.state.p_pos = np.random.uniform(-1., +1., world.dim_p)
 
-    def is_collision(self, agent1, agent2):
-        delta_pos = agent1.state.p_pos - agent2.state.p_pos
-        dist = np.sqrt(np.sum(np.square(delta_pos)))
-        dist_min = agent1.size + agent2.size
-        return True if dist < dist_min else False
+    def check_target_dist(self, world, th):
+        for i, landmark_i in enumerate(world.landmarks):
+            pos_i = landmark_i.state.p_pos
+            for j, landmark_j in enumerate(world.landmarks):
+                if i != j:
+                    pos_j = landmark_j.state.p_pos
+                    dist = np.linalg.norm(pos_i - pos_j)
+                    if dist <= th:
+                        return False
+        return True
 
     def reward(self, agent, world):
-        # Agents are rewarded based on minimum agent distance to each landmark, penalized for collisions
-        rew = 0
+        """Agents are rewarded based on minimum agent distance 
+        to each landmark
+        """
+        reward = 0.
         for l in world.landmarks:
             dists = [np.sqrt(np.sum(np.square(a.state.p_pos - l.state.p_pos))) for a in world.agents]
-            rew -= min(dists)
-        if agent.collide:
-            for a in world.agents:
-                if self.is_collision(a, agent):
-                    rew -= 1
-        return rew
+            reward -= min(dists)
+
+        return reward
 
     def observation(self, agent, world):
-        # get positions of all entities in this agent's reference frame
+        """
+        For each agent, observation consists of:
+        [agent vel, agent pos, target relative pos, agent relateive pos, comm]
+        """
         entity_pos = []
         for entity in world.landmarks:  # world.entities:
             entity_pos.append(entity.state.p_pos - agent.state.p_pos)
-        # entity colors
-        entity_color = []
-        for entity in world.landmarks:  # world.entities:
-            entity_color.append(entity.color)
-        # communication of all other agents
+
         comm = []
         other_pos = []
         for other in world.agents:
@@ -97,4 +91,5 @@ class Scenario(BaseScenario):
                 continue
             comm.append(other.state.c)
             other_pos.append(other.state.p_pos - agent.state.p_pos)
+
         return np.concatenate([agent.state.p_vel] + [agent.state.p_pos] + entity_pos + other_pos + comm)
