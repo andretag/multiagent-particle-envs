83664d4f94a06912e0bbe10af9c63b6aca723bcf
Dong Ki Kim <dkkim93@mit.edu>
2019-04-23 12:12:20 -0400

    Added discrete_action flag to be more flexible

--- multiagent/environment.py
+++ multiagent/environment.py
@@ -15,7 +15,7 @@ class MultiAgentEnv(gym.Env):
 
     def __init__(self, world, reset_callback=None, reward_callback=None,
                  observation_callback=None, info_callback=None,
-                 done_callback=None, shared_viewer=True):
+                 done_callback=None, shared_viewer=True, discrete_action_space=False):
         self.world = world
         self.agents = self.world.policy_agents
         # set required vectorized gym env property
@@ -27,7 +27,7 @@ class MultiAgentEnv(gym.Env):
         self.info_callback = info_callback
         self.done_callback = done_callback
         # environment parameters
-        self.discrete_action_space = False  # NOTE For MADDPG, this has to be set to True
+        self.discrete_action_space = discrete_action_space  # NOTE For MADDPG (discrete), this has to be set to True
         # if true, action is a number 0...N, otherwise action is a one-hot N-dimensional vector
         self.discrete_action_input = False
         # if true, even the action is continuous, action will be performed discretely
