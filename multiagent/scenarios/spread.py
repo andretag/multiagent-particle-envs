3de3f683ceedddf64649baf18e3336aff22005f5
Dong-Ki Kim <dkkim93@mit.edu>
2019-12-28 18:12:18 -0500

    meta branch added

--- multiagent/scenarios/spread.py
+++ multiagent/scenarios/spread.py
@@ -1,45 +1,35 @@
 import numpy as np
-from multiagent.core import World, Agent, Landmark, Goal
+from multiagent.core import World, Agent, Landmark
 from multiagent.scenario import BaseScenario
 
 
 class Scenario(BaseScenario):
-    def make_world(self):
+    def make_world(self, args):
+        # Set any world properties first
         world = World()
-        # set any world properties first
-        world.dim_c = 2
-        num_agents = 2
-        num_landmarks = num_agents
-        num_goals = num_agents
+        world.dim_c = args.n_agent
         world.collaborative = True
 
-        # add agents
-        world.agents = [Agent() for i in range(num_agents)]
+        # Add agents
+        world.agents = [Agent() for i in range(args.n_agent)]
         for i, agent in enumerate(world.agents):
             agent.name = 'agent %d' % i
             agent.collide = False
             agent.silent = True
             agent.size = 0.10
 
-        # add landmarks
-        world.landmarks = [Landmark() for i in range(num_landmarks)]
+        # Add landmarks
+        world.landmarks = [Landmark() for i in range(args.n_agent)]
         for i, landmark in enumerate(world.landmarks):
             landmark.name = 'landmark %d' % i
             landmark.collide = False
-            landmark.movable = False
+            landmark.movable = False 
 
-        # add goals (used only for vis)
-        world.goals = [Goal() for i in range(num_goals)]
-        for i, goal in enumerate(world.goals):
-            goal.name = 'goal %d' % i
-            goal.collide = False
-            goal.movable = False
-
-        # make initial conditions
         self.reset_world(world)
         return world
 
     def reset_world(self, world):
+        # TODO Same initial pos
         for i, agent in enumerate(world.agents):
             if i == 0:
                 agent.color = np.array([1.0, 0.0, 0.0])
@@ -47,23 +37,18 @@ class Scenario(BaseScenario):
                 agent.color = np.array([0.0, 1.0, 0.0])
             else:
                 raise NotImplementedError()
-            agent.state.p_pos = np.random.uniform(-1, +1, world.dim_p)
+            agent.state.p_pos = np.array([0., 0.])
             agent.state.p_vel = np.zeros(world.dim_p)
             agent.state.c = np.zeros(world.dim_c)
 
         for i, landmark in enumerate(world.landmarks):
             landmark.color = np.array([0.25, 0.25, 0.25])
-            landmark.state.p_pos = np.random.uniform(-1, +1, world.dim_p)
+            landmark.state.p_pos = np.random.uniform(-1., +1., world.dim_p)
             landmark.state.p_vel = np.zeros(world.dim_p)
-        while self.check_landmark_dist(world, th=agent.size * 2) is False:
+        while self.check_landmark_dist(world, th=agent.size * 2.) is False:
             for i, landmark in enumerate(world.landmarks):
                 landmark.state.p_pos = np.random.uniform(-1, +1, world.dim_p)
                 landmark.state.p_vel = np.zeros(world.dim_p)
-
-        for i, goal in enumerate(world.goals):
-            goal.color = np.array([0.25, 0.25, 0.25])
-            goal.state.p_pos = np.zeros(world.dim_p) - 2  # NOTE Initialize outside of the box
-            goal.state.p_vel = np.zeros(world.dim_p)
 
     def check_landmark_dist(self, world, th):
         for i, landmark_i in enumerate(world.landmarks):
@@ -107,26 +92,23 @@ class Scenario(BaseScenario):
             dists = [np.sqrt(np.sum(np.square(a.state.p_pos - l.state.p_pos))) for a in world.agents]
             rew -= min(dists)
         if agent.collide:
+            raise ValueError("remove")
             for a in world.agents:
                 if self.is_collision(a, agent):
                     rew -= 1
         return rew
 
     def observation(self, agent, world):
-        # get positions of all entities in this agent's reference frame
-        entity_pos = []
+        agent_poses = []
+        for agent in world.agents:
+            agent_poses.append(agent.state.p_pos)
+
+        agent_vels = []
+        for agent in world.agents:
+            agent_vels.append(agent.state.p_vel)
+
+        entity_poses = []
         for entity in world.landmarks:
-            entity_pos.append(entity.state.p_pos - agent.state.p_pos)
-            # entity_pos.append(entity.state.p_pos)
+            entity_poses.append(entity.state.p_pos)
 
-        # communication of all other agents
-        comm = []
-        other_pos = []
-        for other in world.agents:
-            if other is agent: 
-                continue
-            comm.append(other.state.c)
-            other_pos.append(other.state.p_pos - agent.state.p_pos)
-            # other_pos.append(other.state.p_pos)
-
-        return np.concatenate([agent.state.p_vel] + [agent.state.p_pos] + entity_pos + other_pos)
+        return np.concatenate(agent_poses + agent_vels + entity_poses)
